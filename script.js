4b5e35c4280c7c71257484a91a69b5532fa0aa29
Chaitanya-116 <chaitanyamagapu14@gmail.com>
2025-09-22 21:50:47 +0530

    updates the feature1 branch

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-const container = document.querySelector('.container')
-const unsplashURL = 'https://source.unsplash.com/random/'
-const rows = 5
+const container = document.querySelector('.container');
+const rows = 5;
 
-for(let i = 0; i < rows * 3; i++) {
-    const img = document.createElement('img')
-    img.src = `${unsplashURL}${getRandomSize()}`
-    container.appendChild(img)
-}
-
-function getRandomSize() {
-    return `${getRandomNr()}x${getRandomNr()}`
+for (let i = 0; i < rows * 3; i++) {
+    const img = document.createElement('img');
+    img.src = `https://picsum.photos/${getRandomNr()}/${getRandomNr()}?random=${Date.now() + i}`;
+    img.alt = 'Random Image';
+    container.appendChild(img);
 }
 
 function getRandomNr() {
-    return Math.floor(Math.random() * 10) + 300
+    return Math.floor(Math.random() * 300) + 300; // sizes 300-600
 }
